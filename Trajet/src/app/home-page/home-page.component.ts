9d60ffd69073f64ffe73709868be421bdcd84189
Tauste73 <77971546+Tauste73@users.noreply.github.com>
2023-03-11 19:28:32 +0100

    fix

--- Trajet/src/app/home-page/home-page.component.ts
+++ Trajet/src/app/home-page/home-page.component.ts
@@ -1,33 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ListBornesService } from '../services/list-bornes.service';
-import { ListVehiculeService } from '../services/list-vehicule.service';
-import { TempsTrajetService } from '../services/temps-trajet.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit{
+export class HomePageComponent{
 
 
 
 
-  constructor() {   }
-
-
-
-
-
-
-  ngOnInit(): void {
-    /*this.listVehicule.getListVehicule().subscribe(
-      (data) => {
-        console.log(data.data.vehicleList);
-        this.mylistVehicule = data.data.vehicleList
-      })*/
-
-
-  }
 
 }
